5bc29f23913c6f7855b99a88f9459b44cc832a89
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2025-01-06 20:53:59 +0800

    feat: add css styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const libraryPage = document.getElementById("libraryPage");
 const bookContainer = document.getElementById("book-container");
 const bookForm = document.getElementById("bookForm");
+const addBookModal = document.getElementById("addBookModal");
 
 const library = [];
 
@@ -11,9 +12,20 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
+const STATUS = {
+  readText: "Finished",
+  notReadText: "Not Finished",
+  readClass: "finished",
+  notReadClass: "not-finished",
+};
+
 libraryPage.addEventListener("click", (e) => {
   if (e.target.id === "addBookBtn") {
     addBookModal.showModal();
+  }
+
+  if (e.target.id === "closeButton") {
+    addBookModal.close();
   }
 
   if (e.target.classList.contains("delete-btn")) {
@@ -35,9 +47,9 @@ const getInputData = (form) => {
   const book = Object.fromEntries(formData);
 
   if (book.status === undefined) {
-    book.status = "Not Read";
+    book.status = STATUS.notReadText;
   } else {
-    book.status = "Read";
+    book.status = STATUS.readText;
   }
 
   addBookToLibrary(book.title, book.author, book.pages, book.status);
@@ -52,6 +64,8 @@ const addBookToLibrary = (title, author, pages, readStatus) => {
 };
 
 const deleteBook = (target) => {
+  console.log(target.parentNode);
+
   library.splice(target, 1);
   displayLibrary();
 };
@@ -59,33 +73,40 @@ const deleteBook = (target) => {
 const changeReadStatus = (e) => {
   const book = library[e.target.id];
 
-  if (e.target.textContent === "Not Read") {
-    book.readStatus = "Read";
-    e.target.textContent = "Read";
-    e.target.classList.replace("not-read", "read");
+  if (e.target.textContent === STATUS.notReadText) {
+    book.readStatus = STATUS.readText;
+    e.target.textContent = STATUS.readText;
+    e.target.classList.replace(STATUS.notReadClass, STATUS.readClass);
   } else {
-    book.readStatus = "Not Read";
-    e.target.textContent = "Not Read";
-    e.target.classList.replace("read", "not-read");
+    book.readStatus = STATUS.notReadText;
+    e.target.textContent = STATUS.notReadText;
+    e.target.classList.replace(STATUS.readClass, STATUS.notReadClass);
   }
 };
 
 function createBookElement(elementType, className, text, obj, index) {
   const bookElement = document.createElement(elementType);
   bookElement.textContent = text;
-  bookElement.id = index;
   bookElement.classList.add(className);
 
   let readStatusClass = obj.readStatus;
 
-  if (readStatusClass === "Not Read") {
-    readStatusClass = "not-read";
+  if (index !== undefined) {
+    bookElement.id = index;
+  }
+
+  if (readStatusClass === STATUS.notReadText) {
+    readStatusClass = STATUS.notReadClass;
   } else {
-    readStatusClass = "read";
+    readStatusClass = STATUS.readClass;
   }
 
   if (bookElement.classList.contains("status-btn")) {
-    bookElement.classList.add(readStatusClass);
+    bookElement.classList.add(readStatusClass, "btn");
+  }
+
+  if (bookElement.classList.contains("delete-btn")) {
+    bookElement.classList.add("btn");
   }
 
   return bookElement;
@@ -93,11 +114,11 @@ function createBookElement(elementType, className, text, obj, index) {
 
 const createBookItem = (index, book) => {
   const bookItem = document.createElement("article");
-  bookItem.classList.add("book-item");
+  bookItem.classList.add("book-item", "center");
 
   const title = createBookElement("h2", "title", book.title, book);
-  const author = createBookElement("h3", "author", book.author, book);
-  const pages = createBookElement("p", "pages", book.pages, book);
+  const author = createBookElement("p", "author", `By ${book.author}`, book);
+  const pages = createBookElement("p", "pages", `${book.pages} Pages`, book);
   const statusButton = createBookElement(
     "button",
     "status-btn",
@@ -113,7 +134,11 @@ const createBookItem = (index, book) => {
     index
   );
 
-  bookItem.append(title, author, pages, statusButton, deleteButton);
+  const buttonGroup = document.createElement("div");
+  buttonGroup.classList.add("btn-group", "flex-row", "center");
+
+  buttonGroup.append(statusButton, deleteButton);
+  bookItem.append(title, author, pages, buttonGroup);
 
   return bookItem;
 };
@@ -130,10 +155,12 @@ const displayLibrary = () => {
   const bookFragment = document.createDocumentFragment();
 
   if (library.length > 0) {
+    bookContainer.classList.add("flex-row");
     populateBookContainer(bookFragment, library);
     bookContainer.appendChild(bookFragment);
   } else {
-    bookContainer.innerHTML = "<h2>No books yet</h2>";
+    bookContainer.classList.remove("flex-row");
+    bookContainer.innerHTML = "<h2>Your library is empty</h2>";
   }
 };
 
